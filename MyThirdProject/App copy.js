6a9c330a36dbf5fc97fe05e3a4d51b251be9989a
EdenBruner <130175173+EdenBruner@users.noreply.github.com>
2023-04-30 19:56:31 -0600

    MyThirdProject

---
+++ MyThirdProject/App copy.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+function Student(props) {
+  return (
+    <View>
+      <Text>Hey, My Name is {props.name}, I am a student in CIS340!</Text>
+    </View>
+  );
+}
+
+export default function MultiComp(){
+  return(
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text> Welcome to CIS340 </Text>
+      <Student name="Eden Bruner"/>
+      <Student name="Alice ####"/>
+      <Student name="Bob ####"/>
+      <Student name="Jon Clark"/>
+    </View>
+  )
+}
